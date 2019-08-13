ec569363c70dc5e097ea0dca1f55b9297b37a59c
Eric Fuller <efuller82@gmail.com>
2019-08-13 15:53:50 -0500

    initial file, pseudocode

---
+++ bamazon.js
@@ -0,0 +1,20 @@
+// npms:
+// need inquirer
+//need mysql
+
+// display all items available for sale, including:
+// id, names, prices
+
+// prompt user with two messages
+
+    // ask them the name of the product they'd like to buy
+
+    // it should then ask how many they'd like to buy
+
+// Once order is placed, app should check if there is enough of the product 
+// to fulfill request
+    // if not, log a phrase "insufficient quantity!" then prevent order from going through
+
+// If store does have enough, fill the order:
+    // update the SQL db to reflect the remaining quantity
+    // Once the update goes through, show the customer the total cost of their purchase
