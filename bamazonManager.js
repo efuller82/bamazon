8004d79b2cfe20661fa08f06a930d5295f49ccaa
Eric Fuller <efuller82@gmail.com>
2019-08-15 05:45:02 -0500

    manager display added

---
+++ bamazonManager.js
@@ -0,0 +1,47 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table3');
+
+//connect to db
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "Isaaczoeezra1!",
+    database: "bamazon"
+})
+
+connection.connect();
+
+// Manager should be able to:
+// view products for sale
+// view low inventory <5
+// add to inventory
+// add new product
+
+
+var display = function () {
+    console.log("Connected as id: " + connection.threadId);
+    connection.query("SELECT * FROM products", function (err, result) {
+        if (err) throw err;
+        console.log('');
+        console.log('');
+        // set up table
+        var table = new Table({
+            head: ['ID', 'Product Name', 'Cost', 'Quantity'],
+            colWidths: [8, 30, 10, 10],
+            colAligns: ['center', 'left', 'left', 'center'],
+            style: {
+                head: ['blue'],
+                compact: true
+            }
+        });
+        // loop through db and push results to the table array
+        for (var i = 0; i < result.length; i++) {
+            table.push([result[i].id, result[i].product_name, '$' + result[i].price, result[i].stock_quantity]);
+        }
+        console.log(table.toString());
+        console.log('');
+    })
+};
+display();
