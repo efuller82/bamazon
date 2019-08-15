6eea49024b5eef369b6c675b516eead53a51b5db
Eric Fuller <efuller82@gmail.com>
2019-08-15 05:20:42 -0500

    buy function working; customer side complete

--- bamazon.js
+++ bamazon.js
@@ -22,11 +22,8 @@ var display = function () {
     //display data
     connection.query("SELECT * FROM products", function (err, result) {
         if (err) throw err;
-        console.log();
-        console.log();
-        console.log();
-        console.log();
-
+        console.log('');
+        console.log('');
         // setting up table design
         var table = new Table({
             head: ['ID', 'Product Name', 'Cost'],
@@ -37,6 +34,7 @@ var display = function () {
                 compact: true
             }
         });
+        // Loop through db results and push them to table array
         for (var i = 0; i < result.length; i++) {
             table.push([result[i].id, result[i].product_name, '$' + result[i].price]);
         }
@@ -45,5 +43,55 @@ var display = function () {
     })
 };
 
+var buy = function () {
+    inquirer.prompt({
+        name: "buyProduct",
+        type: "input",
+        message: "Please enter the ID number of the product you wish to buy: "
+    }).then(function (answer) {
+        var selection = answer.buyProduct;
+        connection.query('SELECT * FROM products WHERE ID=?', selection, function (err, result) {
+            if (err) throw err;
+            if (result.length === 0) {
+                console.log("That product does not exist. Please enter a valid ID.")
+
+                buy();
+            } else {
+                // inquirer about quantity of purchase
+                inquirer.prompt({
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to buy?"
+                }).then(function (answerTwo) {
+                    // this will handle if there is enough inventory
+                    var quantity = answerTwo.quantity;
+                    if (quantity > result[0].stock_quantity) {
+                        console.log("Derp! There are only " + result[0].stock_quantity + " remaining in inventory.  Please try again.")
+                        buy();
+                        // if there is enough quantity
+                    } else {
+                        console.log("Purchased " + quantity + " " + result[0].product_name + " @ $" + result[0].price);
+                        console.log("____________________________");
+                        console.log("Total: " + "$" + (quantity * result[0].price).toFixed(2));
+                        // connect to database to update quantitt
+                        var newQuantity = result[0].stock_quantity - quantity;
+                        connection.query(
+                            "UPDATE products SET stock_quantity = " + newQuantity + " WHERE id = " + result[0].id, function (err, resultUpdate) {
+                                if (err) throw err;
+                                console.log("____________________________");
+                                console.log("\rWe appreciate your order." + "\rWe will now exploit our workers and the USPS on your behalf.");
+                                connection.end();
+                            }
+                        )
+                    }
+
+                })
+            }
+        });
+    });
+};
+
+
 display();
+buy();
 
